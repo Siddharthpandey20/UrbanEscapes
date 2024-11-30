ed10e4eca7712a2dff6d343410b166b37fcda4f0
Siddharthpandey20 <siddharthpandey2611@gmail.com>
2024-11-30 14:53:14 +0530

    first commit

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const {connectToMongoDB} = require("./connection")
 const LIST = require("./models/listing")
 
 //ConnectToMongoDB
-connectToMongoDB(process.env.MONGO_URL)
+connectToMongoDB("mongodb://127.0.0.1:27017/project")
 .then(()=>console.log("MongoDB Successfully Connected"))
 
 //Setup EJS
